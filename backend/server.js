bb6794205f1fcbf20ac9a74bcc81b03900c08533
losarwarameya <losarwarah@gmail.com>
2024-03-11 20:15:46 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -13,6 +14,8 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
@@ -24,6 +27,12 @@ app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
